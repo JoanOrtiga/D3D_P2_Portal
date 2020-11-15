186c667da12eb2a7997a0ae48df563b138b8f57f
Marc <marcgaldo.97@gmail.com>
2020-11-15 22:06:07 +0100

    .

--- Assets/Code/SpawnCubes.cs
+++ Assets/Code/SpawnCubes.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class SpawnCubes : MonoBehaviour
 {
+    private bool pulsado;
     public CubeSpawner spawner;
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (Input.GetKeyDown(KeyCode.F))
-            {
+            if (pulsado)
                 spawner.SpawnCube();
-            }
+
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            pulsado = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.F))
+        {
+            pulsado = false;
         }
     }
 }
